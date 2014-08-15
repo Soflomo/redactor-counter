4dfb7eef6b19ef22fe6ee2cd8193988b835c756f
Jurre Antonisse <jurre@soflomo.com>
2014-08-15 09:21:30 +0200

    Build in dist dir, remove renamer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulpFilter = require('gulp-filter');
 var gulp       = require('gulp');
 var uglify     = require('gulp-uglify');
-var rename     = require('gulp-rename');
 
 gulp.task('build', function(){
     gulp.src('*')
         .pipe(gulpFilter('redactor-counter.js'))
         .pipe(uglify())
-        .pipe(rename('redactor-counter.min.js'))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./dist/'));
 });
